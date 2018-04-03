c88e9252b0493db04e7e1ab907fcc84503832004
steelbrain <i@steelbrain.me>
2018-04-03 19:25:14 +0400

    :new: Add scripts and main entry point

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+async function main() {
+  // TODO: Do something here
+  console.log('Hello World')
+}
+
+main().catch(error => {
+  console.error(error && error.stack)
+  process.exit(1)
+})
