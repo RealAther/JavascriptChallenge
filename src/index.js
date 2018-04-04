d31a267511956786f35da3b3f9126bbe134b480b
steelbrain <i@steelbrain.me>
2018-04-04 16:10:58 +0400

    :new: Add basic routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 // @flow
 
+import url from 'url'
 import http from 'http'
+import querystring from 'querystring'
 
 let userCount = 0
 
-async function main() {
-  http
-    .createServer(function(request, response) {
-      userCount++
-      response.write(`Hello World!:  We have had ${userCount} visits!`)
-      response.end()
+const ROUTES = {
+  '/': function(request, response, parsed) {
+    userCount++
+    response.write(`Hello World!:  We have had ${userCount} visits!`)
+    response.end()
+    console.log('handling request', parsed)
+  },
+}
+
+function handleConnection(request, response) {
+  const parsedUrl = url.parse(request.url)
+  if (ROUTES[parsedUrl.pathname]) {
+    ROUTES[parsedUrl.pathname](request, response, {
+      path: parsedUrl.pathname,
+      query: querystring.parse(parsedUrl.query || ''),
     })
-    .listen(8080)
+  } else {
+    response.statusCode = 404
+    response.end(`Requested resource at '${parsedUrl.pathname}' was not found`)
+  }
+}
+
+async function main() {
+  await new Promise((resolve, reject) => {
+    http
+      .createServer(handleConnection)
+      .listen(8080, resolve)
+      .on('error', reject)
+  })
 }
 
 main().catch(error => {
