a6af9ad261e9211e325a7115a4353d4881884609
Real-Ather <real.ather@gmail.com>
2018-04-05 20:26:45 +0500

    :new: Add Express and served local files

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 // @flow
 
-import url from 'url'
-import http from 'http'
-import querystring from 'querystring'
-
-let userCount = 0
-
-const ROUTES = {
-  '/': function(request, response, parsed) {
-    userCount++
-    response.write(`Hello World!:  We have had ${userCount} visits!`)
-    response.end()
-    console.log('handling request', parsed)
-  },
-}
-
-function handleConnection(request, response) {
-  const parsedUrl = url.parse(request.url)
-  if (ROUTES[parsedUrl.pathname]) {
-    ROUTES[parsedUrl.pathname](request, response, {
-      path: parsedUrl.pathname,
-      query: querystring.parse(parsedUrl.query || ''),
-    })
-  } else {
-    response.statusCode = 404
-    response.end(`Requested resource at '${parsedUrl.pathname}' was not found`)
-  }
-}
+import express from 'express'
 
 async function main() {
-  await new Promise((resolve, reject) => {
-    http
-      .createServer(handleConnection)
-      .listen(8080, resolve)
-      .on('error', reject)
-  })
+  express()
+    .use(express.static('./public'))
+    .listen(8080)
 }
 
 main().catch(error => {
