8ef66a4b33bc9e4b4260e63bcd337d04f1cfd28f
Real-Ather <real.ather@gmail.com>
2018-04-14 04:30:05 +0500

    :new: serving stylesheet

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import express from 'express'
 async function main() {
   express()
     .use(express.static('./public'))
+    .get('/', function(req, res) {
+      res.end(`
+        <!DOCTYPE HTML>
+        <html>
+          <head>
+            <link rel="stylesheet" href="style.css" type="text/css">
+          </head>
+          <body>
+            Hello World
+          </body>
+        </html>
+      `)
+    })
     .listen(8080)
 }
 
