5a165b4d3d85df334de4b845d6d7bd16dc61c860
Real-Ather <real.ather@gmail.com>
2018-04-15 04:56:29 +0500

    :new: added submit form :new:

--- src/index.js
+++ src/index.js
@@ -6,33 +6,39 @@ import bodyParser from 'body-parser'
 let userCount = 0
 let array = []
 
+function renderPage(req, res) {
+  res.end(`
+    <!DOCTYPE HTML>
+    <html>
+      <head>
+        <link rel="stylesheet" href="style.css" type="text/css">
+      </head>
+      <body>
+        Hello World!: You have had ${userCount} visits.
+        <form method="post">
+          <div class="text-container">
+            <textarea name="content" class="text-area" placeholder="Write something here!"></textarea>
+            <button class="submit">Submit</button>
+          </div>
+        </form>
+        Previously sent stuff:
+        <pre>${array.join('\n')}</pre>
+      </body>
+    </html>
+  `)
+}
+
 async function main() {
   express()
     .use(bodyParser.urlencoded({ extended: true }))
     .use(express.static('./public'))
     .post('/', function(req, res) {
-      // console.log(req.body)
-      array.push(req.body[array])
+      array.push(req.body.content)
+      renderPage(req, res)
     })
     .get('/', function(req, res) {
       userCount++
-      res.end(`
-        <!DOCTYPE HTML>
-        <html>
-          <head>
-            <link rel="stylesheet" href="style.css" type="text/css">
-          </head>
-          <body>
-            Hello World!: You have had ${userCount} visits.
-            <form method="post">
-              <div class="text-container">
-                <textarea name="Text-Area" class="text-area" placeholder="Write something here!"></textarea>
-                <button class="submit">Submit</button>
-              </div>
-            </form>
-          </body>
-        </html>
-      `)
+      renderPage(req, res)
     })
     .listen(8080)
 }
