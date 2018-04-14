dd76373a4d175281f45fc881451fce8c593a744e
Real-Ather <real.ather@gmail.com>
2018-04-15 04:47:59 +0500

    boo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 let userCount = 0
+let array = []
 
 async function main() {
   express()
     .use(bodyParser.urlencoded({ extended: true }))
     .use(express.static('./public'))
+    .post('/', function(req, res) {
+      // console.log(req.body)
+      array.push(req.body[array])
+    })
     .get('/', function(req, res) {
       userCount++
       res.end(`
@@ -19,6 +24,12 @@ async function main() {
           </head>
           <body>
             Hello World!: You have had ${userCount} visits.
+            <form method="post">
+              <div class="text-container">
+                <textarea name="Text-Area" class="text-area" placeholder="Write something here!"></textarea>
+                <button class="submit">Submit</button>
+              </div>
+            </form>
           </body>
         </html>
       `)
