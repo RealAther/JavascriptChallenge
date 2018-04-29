20d04e0def3bfeff5f75b542164d14bf81ad5d08
Real-Ather <real.ather@gmail.com>
2018-04-30 02:52:02 +0500

    :new: simplifying .filter

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,6 @@ import path from 'path'
 import Sequelize from 'sequelize'
 import configManifest from '../../config/config.json'
 
-const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 const config = configManifest[env]
 const db = {}
@@ -12,7 +11,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 fs
   .readdirSync(__dirname)
-  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+  .filter(file => file !== '.js' && file.endsWith('.js') && file !== 'index.js')
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
